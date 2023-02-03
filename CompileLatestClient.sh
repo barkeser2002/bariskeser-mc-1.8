7e53b101d5e721672b2880400420b69452d08736
barkeser2002 <bkeser10@gmail.com>
2023-02-04 02:38:27 +0300

    first commit

---
+++ CompileLatestClient.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/home/bariskeser/projects/java11/bin/java -cp "buildtools/BuildTools.jar" net.lax1dude.eaglercraft.v1_8.buildtools.gui.CompileLatestClientGUI
+rm -rf "##TEAVM.TMP##"
---
+++ sources/main/java/net/lax1dude/eaglercraft/v1_8/EaglercraftVersion.java
@@ -0,0 +1,46 @@
+package net.lax1dude.eaglercraft.v1_8;
+
+public class EaglercraftVersion {
+	
+	
+	//////////////////////////////////////////////////////////////////////
+	
+	/// Customize these to fit your fork:
+	
+	public static final String projectForkName = "BarisKeser-Server";
+	public static final String projectForkVersion = "u16";
+	public static final String projectForkVendor = "barkeser2002";
+	
+	public static final String projectForkURL = "https://bariskeser.com";
+	
+	//////////////////////////////////////////////////////////////////////
+	
+	
+	
+	// Do not change these, they must stay as credit to lax1dude's
+	// original repository for maintaining the project:
+
+	public static final String projectOriginName = "BarisKeser-Server";
+	public static final String projectOriginAuthor = "barkeser2002";
+	public static final String projectOriginRevision = "1.8";
+	public static final String projectOriginVersion = "u16";
+	
+	public static final String projectOriginURL = "https://bariskeser.com";
+	
+	
+	// Miscellaneous variables:
+
+	public static final String mainMenuStringA = "Minecraft 1.8.8";
+	public static final String mainMenuStringB = projectOriginName + " " +
+			projectOriginRevision + "-" + projectOriginVersion;
+	public static final String mainMenuStringC = null;
+	public static final String mainMenuStringD = "Telif Hakkı Mojang AB";
+
+	public static final String mainMenuStringE = projectForkName + " " + projectForkVersion;
+	public static final String mainMenuStringF = "Yapan kişi " + projectForkVendor;
+
+	public static final boolean mainMenuEnableGithubButton = true;
+	
+	
+
+}
